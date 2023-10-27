d89ba70ddaa683de1baa21c2807ecdc14748874c
vicentefiorito <vicentefiorito@gmail.com>
2023-10-27 09:12:08 -0400

    first commit

---
+++ bst.js
@@ -0,0 +1,20 @@
+// Node class
+class Node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+// Tree class
+class Tree{
+    constructor() {
+        this.root = null
+    }
+}
+
+// testing 
+const test1 = new Node(5)
+
+console.log(test1)
