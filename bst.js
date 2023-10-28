753f9acaa3b3ec07bfab1af44b653e3bc6e5488d
vicentefiorito <vicentefiorito@gmail.com>
2023-10-28 10:11:29 -0400

    starting to build the methods to build the full BST, starting with sorting the array

--- bst.js
+++ bst.js
@@ -7,14 +7,26 @@ class Node{
     }
 }
 
-// Tree class
+// Tree Class
 class Tree{
-    constructor() {
-        this.root = null
+    constructor(arr){
+        this.root = this.buildTree(arr)
+    }
+
+    // builds the tree
+    buildTree(arr){
+        const sortedArr = this.sortArray(arr)
+        return sortedArr
+    }
+
+    // sorts the array
+    sortArray(arr) {
+        const sorted = arr.sort((a,b) => a-b)
+        return sorted
     }
 }
-
 // testing 
-const test1 = new Node(5)
+arr = [5,8,34,23,56,4]
+tree = new Tree(arr)
+console.log(tree)
 
-console.log(test1)
