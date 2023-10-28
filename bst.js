51f8b53924f82b82b7ad2f0db796414f05e07fb8
vicentefiorito <vicentefiorito@gmail.com>
2023-10-28 11:29:20 -0400

    managed to implement the BST after sorting and remove duplicates

--- bst.js
+++ bst.js
@@ -19,9 +19,28 @@ class Tree{
         const sortedArr = this.sortArray(arr)
         // remove duplicates from the sorted array
         const uniqueArray = this.removeDuplicates(sortedArr)
-        // this gets the length of the array for generation of nodes
-        
-        return sortedArr
+        // this gets the array length to be used for the generations of nodes
+        const n = uniqueArray.length
+
+        // add nodes to the tree
+        const root = this.addNode(uniqueArray,0,n-1)
+        return root
+    }
+
+    // this function puts nodes into the tree
+    addNode(arr,start,end) {
+        // if we have ran through the whole array
+        if(start > end) return null
+        // gets the middle element
+        const mid = parseInt((start + end)/2,10)
+
+        // generates the new node to be inserted into the tree
+        const node = new Node(arr[mid])
+
+        // sets the left and the right childs recursively
+        node.left = this.addNode(arr,start,mid-1)
+        node.right = this.addNode(arr,mid+1,end)
+        return node
     }
 
     // sorts the array
@@ -36,8 +55,22 @@ class Tree{
         return uniques
     }
 }
+
+// visualization method
+const prettyPrint = (root, prefix = "", isLeft = true) => {
+    if (root === null) {
+      return;
+    }
+    if (root.right !== null) {
+      prettyPrint(root.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${root.value}`);
+    if (root.left !== null) {
+      prettyPrint(root.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
 // testing 
 arr = [5,8,34,23,56,4,5,6,8,63,45]
 tree = new Tree(arr)
-console.log(tree)
-
+prettyPrint(tree.root)
